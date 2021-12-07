71c4ea6edcee47427f4eb1280d9e775b44fd52cc
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-12-07 09:34:01 +0300

    test9

--- src/main/java/com/sidorin/testapp/controller/MainController.java
+++ src/main/java/com/sidorin/testapp/controller/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String main(){
         return "index.html";
     }
